a1986d2a567c9a36a33066919f258b0ae3a7801a
Ruslan <hi@ruslanpashkov.com>
2025-02-03 03:03:25 +0200

    chore(react-hooks): add type declarations

---
+++ src/declarations.d.ts
@@ -0,0 +1,21 @@
+// TODO: Remove when https://github.com/facebook/react/issues/30119 is resolved
+
+declare module "eslint-plugin-react-hooks" {
+  import type { Linter, Rule } from "eslint";
+
+  export const configs: {
+    recommended: Linter.Config;
+  };
+
+  declare const rules: {
+    "rules-of-hooks": Rule.RuleModule;
+    "exhaustive-deps": Rule.RuleModule;
+  };
+
+  declare const plugin: {
+    configs: typeof configs;
+    rules: typeof rules;
+  };
+
+  export default plugin;
+}
